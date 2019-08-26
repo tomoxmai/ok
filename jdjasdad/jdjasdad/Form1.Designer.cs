853caf1a846be64f649d358c9ed8b98e4c2ae3f1
Alumno <Alumno@PC15>
2019-08-26 09:22:46 -0500

    dzzzxzz

---
+++ jdjasdad/jdjasdad/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace jdjasdad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.box = new System.Windows.Forms.TextBox();
+            this.usu = new System.Windows.Forms.TextBox();
+            this.entrar = new System.Windows.Forms.Button();
+            this.salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(490, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sistema Super Secreto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(45, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Usuario";
+            // 
+            // box
+            // 
+            this.box.Location = new System.Drawing.Point(194, 187);
+            this.box.Name = "box";
+            this.box.Size = new System.Drawing.Size(320, 20);
+            this.box.TabIndex = 3;
+            // 
+            // usu
+            // 
+            this.usu.Location = new System.Drawing.Point(194, 257);
+            this.usu.Name = "usu";
+            this.usu.PasswordChar = '*';
+            this.usu.Size = new System.Drawing.Size(320, 20);
+            this.usu.TabIndex = 4;
+            // 
+            // entrar
+            // 
+            this.entrar.Location = new System.Drawing.Point(25, 352);
+            this.entrar.Name = "entrar";
+            this.entrar.Size = new System.Drawing.Size(100, 86);
+            this.entrar.TabIndex = 5;
+            this.entrar.Text = "Entrar";
+            this.entrar.UseVisualStyleBackColor = true;
+            this.entrar.Click += new System.EventHandler(this.Entrar_Click);
+            // 
+            // salir
+            // 
+            this.salir.Location = new System.Drawing.Point(432, 352);
+            this.salir.Name = "salir";
+            this.salir.Size = new System.Drawing.Size(100, 86);
+            this.salir.TabIndex = 6;
+            this.salir.Text = "Salir";
+            this.salir.UseVisualStyleBackColor = true;
+            this.salir.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 450);
+            this.Controls.Add(this.salir);
+            this.Controls.Add(this.entrar);
+            this.Controls.Add(this.usu);
+            this.Controls.Add(this.box);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox box;
+        private System.Windows.Forms.TextBox usu;
+        private System.Windows.Forms.Button entrar;
+        private System.Windows.Forms.Button salir;
+    }
+}
+
---
+++ jdjasdad/jdjasdad/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jdjasdad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.Close();//cierra el formulario
+        }
+
+        private void Entrar_Click(object sender, EventArgs e)
+        {
+            if(box.Text=="juancho" &&usu.Text =="123tamarindo")
+
+            {
+                MessageBox.Show("jasdjd");
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña no valido");
+                box.Text = "";
+                usu.Text = "";
+                box.Focus();
+            }
+        }
+    }
+}
